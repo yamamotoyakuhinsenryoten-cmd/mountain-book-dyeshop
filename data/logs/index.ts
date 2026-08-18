a0758fc9a60bf719c18b92a71ef28d448c5114e9
mountainbook <yamamoto.yakuhin.senryoten@gmail.com>
2026-08-19 06:47:37 +0900

    ログ追加

--- data/logs/index.ts
+++ data/logs/index.ts
@@ -32,6 +32,7 @@ import { defectbeansai002 } from "./defect-beans-ai-002";
 import { householdbudget001 } from "./house-hold-budget-001";
 import { morningpageocr001 } from "./morningpage-ocr-001";
 import { allseasonscoffee001 } from "./all-seasons-coffee-001";
+import { bench001 } from "./bench-001";
 import { migrateLegacyLog, type Log } from "./types";
 
 const legacyLogs = [
@@ -72,5 +73,6 @@ const newLogs: Log[] = [
   baisen010,
   baisen011,
   aroha002,
+  bench001,
 ];
 export const logs: Log[] = [...legacyLogs.map(migrateLegacyLog), ...newLogs];
